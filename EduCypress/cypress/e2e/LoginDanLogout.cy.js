60c86c1151e1191d0b60b55fa5a090db7be1847d
AldyProject <aldysuryanto001@gmail.com>
2024-10-14 20:19:09 +0700

    Create LoginDanLogout.cy.js

---
+++ EduCypress/cypress/e2e/LoginDanLogout.cy.js
@@ -0,0 +1,11 @@
+describe('Working with inputs', () => {
+    it('Successfull login', () => {
+        cy.visit('http://zero.webappsecurity.com/login.html')
+        cy.get('#user_login').type('username');
+        cy.get('#user_password').type('password');
+        cy.get('#user_remember_me').click();
+        cy.get('.btn.btn-primary').click();
+        cy.get(':nth-child(3) > .dropdown-toggle > .caret').click();
+        cy.get('#logout_link').click();
+    });
+});
