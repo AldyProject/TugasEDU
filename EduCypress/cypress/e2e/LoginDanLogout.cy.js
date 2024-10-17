8237697dbc9d87fcb423826b04021ee5465c94cc
AldyProject <aldysuryanto001@gmail.com>
2024-10-17 20:16:53 +0700

    Update LoginDanLogout.cy.js

--- EduCypress/cypress/e2e/LoginDanLogout.cy.js
+++ EduCypress/cypress/e2e/LoginDanLogout.cy.js
@@ -1,32 +1,33 @@
-describe('Working with login and logout', () => {
-    
-    let value; 
+describe('Working login and logout', () => {
     beforeEach(() => {
-      value = 0; 
-    });
-  
-    it('Visit website', () => {
       cy.visit('http://zero.webappsecurity.com/login.html');
     });
-  
-    // Login user
+    // visit website
+    it('Should visit website', () => {
+        cy.visit('http://zero.webappsecurity.com/login.html');
+      });
+    //Login successfully
     it('Should successfully log in', () => {
-      cy.get('#user_login').type('username');
+      cy.get('#user_login').type('username'); 
       cy.get('#user_password').type('password');
       cy.get('#user_remember_me').click();
       cy.get('.btn.btn-primary').click();
-      
-      // Optionally check if login was successful
-      cy.url().should('include', 'account-summary.html'); // Adjust the URL as needed
-    });
   
-    // Logout user
+      cy.url().should('include', 'account-summary.html'); 
+    });
+    
+    //Logout successfully
     it('Should successfully log out', () => {
+        //Login username and password
+      cy.get('#user_login').type('username'); 
+      cy.get('#user_password').type('password');
+      cy.get('#user_remember_me').click();
+      cy.get('.btn.btn-primary').click();
+      //Logout account summary
       cy.get(':nth-child(3) > .dropdown-toggle > .caret').click();
       cy.get('#logout_link').click();
-      
-      // Optionally check if logout was successful
-      cy.url().should('include', 'login.html'); // Adjust the URL as needed
+  
+      cy.url().should('include', 'index.html'); 
     });
   });
   
