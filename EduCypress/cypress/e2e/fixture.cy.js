36b17e21834906b8b8471102209ba330395fb4e5
Hendrich <hendri.christianto24@gmail.com>
2024-10-17 20:27:43 +0700

    fix file searchbox dan fixture intercept script

--- EduCypress/cypress/e2e/fixture.cy.js
+++ EduCypress/cypress/e2e/fixture.cy.js
@@ -1,57 +1,65 @@
-describe('Working with inputs', () => {
-    it('Successfull login and checkout', () => {
-          cy.visit("https://www.saucedemo.com/" , { timeout: 120000, waitForAnimations: false });
-              cy.fixture("admin").then( admin => {
-                const username1 = admin.username1
-                const password1 = admin.password1
-                const FName1 = admin.FName1
-                const LName1 = admin.LName1
-                const Zip1 = admin.Zip1
-                //input username
-                cy.get('#user-name.input_error.form_input').clear()
-                cy.get('#user-name.input_error.form_input').type(username1)
-                //input password
-                cy.get('#password.input_error.form_input').clear()
-                cy.get('#password.input_error.form_input').type(password1)
-                //Click login
-                cy.get('#login-button.submit-button.btn_action').click()  
-                
-                //Click add to cart
-                cy.get('#add-to-cart-sauce-labs-backpack').click()
-                cy.get('#add-to-cart-sauce-labs-bike-light').click()
+describe("Working with inputs", () => {
+  it("Successfull login and checkout", () => {
+    // Intercept request dan balas dengan sukses palsu
+    cy.intercept("POST", "**/events.backtrace.io/**", (req) => {
+      req.reply(200, {}); // Membalas dengan respons sukses palsu
+    });
+    cy.visit("https://www.saucedemo.com/", {
+      timeout: 120000,
+      waitForAnimations: false,
+    });
+    cy.fixture("admin").then((admin) => {
+      const username1 = admin.username1;
+      const password1 = admin.password1;
+      const FName1 = admin.FName1;
+      const LName1 = admin.LName1;
+      const Zip1 = admin.Zip1;
+      //input username
+      cy.get("#user-name.input_error.form_input").clear();
+      cy.get("#user-name.input_error.form_input").type(username1);
+      //input password
+      cy.get("#password.input_error.form_input").clear();
+      cy.get("#password.input_error.form_input").type(password1);
+      //Click login
+      cy.get("#login-button.submit-button.btn_action").click();
 
-             // Click cart
-             cy.get('[data-test="shopping-cart-link"]').click()
-               //Click button countinue
-                cy.get('#checkout.btn.btn_action.btn_medium.checkout_button').click()
+      //Click add to cart
+      cy.get("#add-to-cart-sauce-labs-backpack").click();
+      cy.get("#add-to-cart-sauce-labs-bike-light").click();
 
-               //Input Firstname, Lastname and Zip/postal
-               //input Firstname
-               cy.get('[data-test="firstName"]').clear()
-               cy.get('[data-test="firstName"]').type(FName1)
+      // Click cart
+      cy.get('[data-test="shopping-cart-link"]').click();
+      //Click button countinue
+      cy.get("#checkout.btn.btn_action.btn_medium.checkout_button").click();
 
-                //input LastName
-                cy.get('[data-test="lastName"]').clear()
-                cy.get('[data-test="lastName"]').type(LName1)
+      //Input Firstname, Lastname and Zip/postal
+      //input Firstname
+      cy.get('[data-test="firstName"]').clear();
+      cy.get('[data-test="firstName"]').type(FName1);
 
-                //input Zip/postal
-                cy.get('[data-test="postalCode"]').clear()
-                cy.get('[data-test="postalCode"]').type(Zip1)
+      //input LastName
+      cy.get('[data-test="lastName"]').clear();
+      cy.get('[data-test="lastName"]').type(LName1);
 
-                //Click continue
-                cy.get('#continue.submit-button.btn.btn_primary.cart_button.btn_action').click()
+      //input Zip/postal
+      cy.get('[data-test="postalCode"]').clear();
+      cy.get('[data-test="postalCode"]').type(Zip1);
 
-                //Click Finish
-                cy.get('[data-test="finish"]').click()
+      //Click continue
+      cy.get(
+        "#continue.submit-button.btn.btn_primary.cart_button.btn_action"
+      ).click();
 
-                //Click Back Home
-                cy.get('#back-to-products.btn.btn_primary.btn_small').click()
-                //Click burger menu
-                cy.get('#react-burger-menu-btn').click()
+      //Click Finish
+      cy.get('[data-test="finish"]').click();
 
-                //Click Logout
-                cy.get('#logout_sidebar_link.bm-item.menu-item').click()
-                })
-                
-            });
-        });
+      //Click Back Home
+      cy.get("#back-to-products.btn.btn_primary.btn_small").click();
+      //Click burger menu
+      cy.get("#react-burger-menu-btn").click();
+
+      //Click Logout
+      cy.get("#logout_sidebar_link.bm-item.menu-item").click();
+    });
+  });
+});
--- EduCypress/cypress/e2e/searchbox.cy.js
+++ EduCypress/cypress/e2e/searchbox.cy.js
@@ -1,9 +1,11 @@
-describe ('Searchbox Test', () =>{
-    before(() => {
-        cy.visit ('http://zero.webappsecurity.com/')
-    });
-    it('Should type into searchbox and submit', () => {
-        cy.get('#searchTerm').type('online {enter}')
-        cy.get('h2').contains('Search Results:')
-    });
-})
+describe("Searchbox Test", () => {
+  before(() => {
+    cy.visit("http://zero.webappsecurity.com/");
+  });
+  it("Should type into searchbox and submit", () => {
+    cy.get("#searchTerm").type("online {enter}");
+    cy.get("h2").contains("Search Results:");
+    cy.get("a").contains("Zero - Free Access to Online Banking");
+    cy.get("a").contains("Zero - Online Statements");
+  });
+});
