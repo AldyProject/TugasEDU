c1aa912e7217d47a425043da38b18a4267544cb2
AldyProject <aldysuryanto001@gmail.com>
2024-10-17 21:57:12 +0700

    Update navbar.cy.js

--- EduCypress/cypress/e2e/navbar.cy.js
+++ EduCypress/cypress/e2e/navbar.cy.js
@@ -1,14 +1,30 @@
 describe('Working with inputs', () => {
-    it('Tugas Navbar', () => {
-        cy.visit('http://zero.webappsecurity.com/')
+    beforeEach(() => {
+        cy.visit('http://zero.webappsecurity.com/');
+      });
+    it('Visit website', () => {
+        cy.visit('http://zero.webappsecurity.com/');
+    })
+    it('Navbar Home page',() => {
         cy.get('#onlineBankingMenu > div > strong').click();
         cy.get('#feedback > div > strong').click();
         cy.get('.brand').click();
+    });
+    it('Successfully Login with valid email and password', () => {
         cy.get('#signin_button').click();
         cy.get('#user_login').type('username');
         cy.get('#user_password').type('password');
         cy.get('#user_remember_me').click();
         cy.get('.btn.btn-primary').click();
+    });
+    it('Successfully Transfer with valid account', () => {
+        //Login account
+        cy.get('#signin_button').click();
+        cy.get('#user_login').type('username');
+        cy.get('#user_password').type('password');
+        cy.get('#user_remember_me').click();
+        cy.get('.btn.btn-primary').click();
+        //Fill data for transfer
         cy.get('#account_activity_tab > a').click();
         cy.get('#transfer_funds_tab > a').click();
         cy.get('select#tf_fromAccountId').select('Credit Card(Avail. balance = $ -265)');
@@ -20,5 +36,6 @@ describe('Working with inputs', () => {
         cy.get('.offset2 > a').click();
         cy.get(':nth-child(3) > .dropdown-toggle > .caret').click();
         cy.get('#logout_link').click();
-    });
+    })   
+    
 });
